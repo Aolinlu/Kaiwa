703115c7f459fcd961c368a18777d49ebf6e44ee
Aolinlu <F_CIL@outlook.com>
2026-07-12 06:20:29 +0800

    feat: add scenario selection flow in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <template>
   <div class="min-h-screen bg-gray-50">
-    <ConversationPage />
+    <ScenarioPage v-if="!currentScenario" @select="startScenario" />
+    <ConversationPage
+      v-else
+      :scenario="currentScenario"
+      @finish="finishSession"
+    />
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
+import ScenarioPage from './pages/ScenarioPage.vue'
 import ConversationPage from './pages/ConversationPage.vue'
+
+const currentScenario = ref(null)
+
+function startScenario(scenario) {
+  currentScenario.value = scenario
+}
+
+function finishSession() {
+  currentScenario.value = null
+}
 </script>
